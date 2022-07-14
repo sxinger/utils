3dcd58dd470f3f4236cd37a5f0df92080c6dc5e4
Song <xsm7f@umsystem.edu>
2022-07-14 01:41:37 -0500

    update extract util function

--- extract_util.R
+++ extract_util.R
@@ -12,23 +12,28 @@ collect_cdm<-function(conn,
                       patset = "PAT_ELIG",
                       key_col_pat = c("PATID"),
                       cdm_schema = "CMS_PCORNET_CDM",
-                      cdm_tbl = "DIAGNOSIS",
-                      vs_url = "",
-                      vs_type = "curated-dx",
-                      vs_file_type = "json",
-                      vs_name = "",
                       key_col_schema=c("PATID"),
+                      cdm_tbl = "DIAGNOSIS",
+                      idx_dt_col = "INDEX_DATE",
+                      ep_dt_col = "EVENT_DATE",
+                      vs_url = "",
+                      vs_template = "curated-dx",
+                      vs_name = "",
                       col_out = NULL,
+                      write_back = FALSE,
+                      home_schema = "PUBLIC",
+                      write_tbl = "TEMP",
                       dry_run = TRUE){
+  
   # get column name of the target cdm table
-  cdm_tbl_nm<-paste0("identifier($",cdm_tbl,")")
   if(is.null(col_out)){
     col_out<-colnames(DBI::dbGetQuery(
-      conn,paste0("select * from ",cdm_schema,".",cdm_tbl_nm," where 1=0")
+      conn,paste0("select * from ",cdm_schema,".",cdm_tbl," where 1=0")
     ))
   }
   col_out<-col_out[!col_out %in% key_col_schema]
-  # contruct matching phrase
+
+  # construct matching phrase
   match_key<-c()
   for(i in seq_along(key_col_pat)){
     match_key<-c(match_key,paste(c(paste0("p.",key_col_pat[i]),
@@ -38,47 +43,50 @@ collect_cdm<-function(conn,
   if(length(key_col_pat)>1){
     match_key<-paste(match_key,collapse = " and ")
   }
+  
   # construct sql baseline 
   sql<-paste0("select distinct ",
               paste0(paste(paste0("p.",key_col_pat,collapse = ","),",",
-                            paste(paste0("f.",col_out,collapse = ",")),
-                            paste0(" from ", patset, " p"),
-                            paste0(" join ", cdm_schema, ".",cdm_tbl_nm," f"),
-                            " on ",match_key)))
-  # specify where clause based on valuset type
+                           paste(paste0("f.",col_out,collapse = ",")),
+                           paste0(",datediff('day',p.",idx_dt_col,",f.",ep_dt_col,") as DAYS_SINCE_INDEX"),
+                           paste0(" from ", home_schema, ".", patset, " p"),
+                           paste0(" join ", cdm_schema, ".", cdm_tbl," f"),
+                           " on ",match_key)))
+  
+  vs_file_type<-gsub(".*\\.","",vs_url)
+  # specify "where" clause based on valuset type
   if (vs_file_type == "json"){
-    vs<-jsonlite::fromJSON(file=vs_url)[[vs_name]]
+    vs<-jsonlite::fromJSON(vs_url)[[vs_name]]
     # curated valueset of icd diagnosis codes
-    if (vs_type == "curated-dx"){
-      sql_where<-"("      
+    if (vs_template == "curated-dx"){
+      sql_where<-""      
       for (icd_vrsn in c("icd9","icd10")){
         for(lev_i in 0:2){
           icd_lev<-paste0("lev",lev_i)
-          if (lev_i==0){
-            sql_where<-paste0(sql_where,"split_part(f.DX,'.',1) in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')")," or ")
-          }else{
-            sql_where<-paste0(sql_where,"substr(f.DX,1,",4+lev_i,") in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')")," or ")
+          if (lev_i==0&&length(vs[[icd_vrsn]][[icd_lev]])>0){
+            sql_where<-paste0(sql_where," or split_part(f.DX,'.',1) in ", 
+                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
+          }else if(length(vs[[icd_vrsn]][[icd_lev]])>0){
+            sql_where<-paste0(sql_where," or substr(f.DX,1,",4+lev_i,") in ", 
+                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
           }
         }
       }
-      sql<-paste0(sql," and ",sql_where,")")
+      sql<-paste0(sql," and (",gsub("^( or)+","",sql_where),")")
+      
     # curated valueset of cpt or icd procedure codes
-    } else if (vs_type == "curated-px"){
+    } else if (vs_template == "curated-px"){
       # icd pcs codes
-      sql_where<-"(" 
-      for(icd_vrsn in c("icd9","icd10")){
+      sql_where<-"" 
+      for(icd_vrsn in c("icd9-pcs","icd10-pcs")){
         for(lev_i in 0:2){
           icd_lev<-paste0("lev",lev_i)
           if(lev_i==0&&length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where,"split_part(f.DX,'.',1) in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')")," or ")
+            sql_where<-paste0(sql_where," or split_part(f.PX,'.',1) in ", 
+                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
           }else if(length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where,"substr(f.DX,1,",4+lev_i,") in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')")," or ")
-          }else{
-            stop("valueset is empty!")
+            sql_where<-paste0(sql_where," or substr(f.PX,1,",3+lev_i,") in ", 
+                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
           }
         }
       # cpt codes
@@ -86,18 +94,18 @@ collect_cdm<-function(conn,
           if(type=="range"&&length(vs[[icd_vrsn]][[icd_lev]])>0){
             rg2vec<-lapply(vs[["hcpcs"]][["range"]],
                            function(x)seq(as.numeric(strsplit(x,"-")[[1]][1]),as.numeric(strsplit(x,"-")[[1]][2])))
-            sql_where<-paste0(sql_where,"f.PX in ", 
-                         paste0("('",paste(unlist(rg2vec),collapse="','"),"')")," or ")
+            sql_where<-paste0(sql_where," or f.PX in ", 
+                         paste0("('",paste(unlist(rg2vec),collapse="','"),"')"))
           }else if(length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where,"f.PX in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')")," or ")
-          }else{
-            stop("valueset is empty!")
+            sql_where<-paste0(sql_where," or f.PX in ", 
+                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
           }
         }
       }
-      sql<-paste0(sql," and ",sql_where,")")
-    } else if (vs_type == "ecqm") {
+      sql<-paste0(sql," and (",gsub("^( or)+","",sql_where),")")
+    
+    # eCQM published valuesets
+    } else if (vs_template == "ecqm") {
       for(cdtype in names(vs[["codelist"]])){
         codelist<-paste0("('",paste(vs[["codelist"]][[cdtype]][,1],collapse="','"),"')")
         # diagnosis
@@ -140,15 +148,19 @@ collect_cdm<-function(conn,
             sql<-paste0(sql," and f.OBSLINC_CODE in ",codelist,
                         "where OBSCLIN_TYPE ='SM'")
           }  
-        }else{
-          stop("ontology not used in CDM!")
         }
       }
-    } else if (vs_type == "ccda"){
+      
+    # CCDA published valuesets
+    } else if (vs_template == "ccda"){
       # TODO
-    } else if (vs_type == "ncbo"){
+    
+    # bioportal search results
+    } else if (vs_template == "ncbo"){
       # TODO
-    } else if (vs_type == "umls"){
+      
+    # UMLS API search results
+    } else if (vs_template == "umls"){
       # TODO
     } else {
       stop("valueset file type not supported!")
@@ -156,22 +168,17 @@ collect_cdm<-function(conn,
   } else if (vs_file_type == "csv"){
     #TODO
   }
+  
   # run query
   if(dry_run==TRUE){
     return(sql)
   }else{
-    DBI::dbGetQuery(conn,sql)
+    if(write_back==TRUE){
+      DBI::dbSendQuery(conn,
+                       paste0("create or replace table ",
+                              home_schema,".",write_tbl," as ",sql))
+    }else{
+      DBI::dbGetQuery(conn,sql)
+    }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
