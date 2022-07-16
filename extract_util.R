84f327c4838e83d54c860fae42e721a067df060a
Song <xsm7f@umsystem.edu>
2022-07-16 01:21:17 -0500

    allow pushing valueset back to database

--- extract_util.R
+++ extract_util.R
@@ -1,77 +1,172 @@
-
 #######################################################
-## project-specific functions for HERON data request ##
+# search CDM table with imported valuesets
 #######################################################
-#Note: all following functions are specific to projects with dependencies
 
-#purpose: write valuesets to database
-#dependency: 1) valueset specified in json file; 2) packages: DBI,jsonlite
-send_valueset<-function(conn,
-                        home_schema = "PUBLIC",
-                        write_tbl = "TEMP",
-                        dry_run = TRUE){
-  
-  vs_file_type<-gsub(".*\\.","",vs_url)
-  # specify "where" clause based on valuset type
-  if (vs_file_type == "json"){
-    vs<-jsonlite::fromJSON(vs_url)[[vs_name]]
-    # curated valueset of icd diagnosis codes
-    if (vs_template == "curated-dx"){
-      
-    # curated valueset of cpt or icd procedure codes
-    } else if (vs_template == "curated-px"){
-    
-    # eCQM published valuesets
-    } else if (vs_template == "ecqm") {
-      
-    # CCDA published valuesets
-    } else if (vs_template == "ccda"){
-      # TODO
-    
-    # bioportal search results
-    } else if (vs_template == "ncbo"){
-      # TODO
-      
-    # UMLS API search results
-    } else if (vs_template == "umls"){
-      # TODO
-    } else {
-      stop("valueset file type not supported!")
-    }
-  } else if (vs_file_type == "csv"){
-    #TODO
-  }
-  
-  # run query
-  if(dry_run==TRUE){
-    return(sql)
+cdm_code_type_map<-function(cdtype){
+  if(grepl("icd9",tolower(cdtype))){
+    return("09")
+  }else if(grepl("icd10",tolower(cdtype))){
+    return("10")
+  }else if(grepl("loinc",tolower(cdtype))){
+    return("LC")
+  }else if(grepl("rxn",tolower(cdtype))){
+    return("RX")
+  }else if(grepl("ndc",tolower(cdtype))){
+    return("ND")
+  }else if(grepl("snom",tolower(cdtype))){
+    return("SM")
+  }else if(grepl("hcpcs",tolower(cdtype))){
+    return("CH")
+  }else if(grepl("cpt",tolower(cdtype))){
+    return("CH")
   }else{
-    DBI::dbSendQuery(conn,
-                     paste0("create or replace table ",
-                            home_schema,".",write_tbl," as ",sql))
+    stop("Standardized value doesn't exist for code type:",cdtype)
   }
 }
 
+load_valueset.curated<-function(vs_url = "",vs_name_str = ""){
+  # load valueset in json
+  vs_file_type<-gsub(".*\\.","=",vs_url)
+  vs_file<-jsonlite::fromJSON(vs_url)
+  
+  # initialize lookup table
+  lookup_tbl<-data.frame(CODE_TYPE=as.character(),
+                         CODE_TYPE_CDM=as.character(),
+                         CODE_SUBTYPE=as.character(),
+                         CODE=as.character(),
+                         CODE_GRP=as.character(),
+                         stringsAsFactors=F)
+  
+  # main code body for parsing json file
+  vs_name_list<-names(vs_file)
+  vs_name_dist<-stringdist(tolower(vs_name_str),vs_name_list, method="jw")
+  vs_name_match<-vs_name_list[which.min(vs_name_dist)]
+  vs<-vs_file[[vs_name_match]]
+  for(cd_type in names(vs)){
+    vs_sub<-vs[[cd_type]]
+    for(cd_subtype in names(vs_sub)){
+      # skip if empty
+      if(length(vs_sub[[cd_subtype]])==0) next
+      # subtype of icd9-cm, icd10-cm codes are: lev0, lev1, lev2
+      if(cd_subtype %in% c('lev0','lev1','lev2')){
+        cd_lst<-vs_sub[[cd_subtype]]
+        # subtypes of hcpcs, icd10-pcs codes are: range, list
+      }else if(cd_subtype=="range"){
+        rg2vec<-lapply(vs_sub[[cd_subtype]],
+                       function(x)seq(as.numeric(strsplit(x,"-")[[1]][1]),as.numeric(strsplit(x,"-")[[1]][2])))
+        cd_lst<-unlist(rg2vec)
+        cd_subtype<-"exact"
+      }else if(cd_subtype %in% c("icd10-pcs","exact")){
+        cd_lst<-vs_sub[[cd_subtype]]
+      }
+      lookup_tbl %<>%
+        bind_rows(data.frame(CODE_TYPE=cd_type,
+                             CODE_TYPE_CDM=cdm_code_type_map(cd_type),
+                             CODE_SUBTYPE=cd_subtype,
+                             CODE=as.character(cd_lst),
+                             CODE_GRP=vs_name_match,
+                             stringsAsFactors = F))
+    }
+  }
+  # return data.frame
+  return(lookup_tbl)
+}
 
-#purpose: collect data from target CDM table based on provided valueset
-#dependency: 1) patient set in database; 2) valueset specified in json file;
-#            3) packages: DBI,jsonlite
+load_valueset.ecqm<-function(vs_url = "",vs_name_str = ""){
+  # load valueset in json
+  vs_file_type<-gsub(".*\\.","=",vs_url)
+  vs_file<-jsonlite::fromJSON(vs_url)
+  
+  # initialize lookup table
+  lookup_tbl<-data.frame(CODE_TYPE=as.character(),
+                         CODE_TYPE_CDM=cdm_code_type_map(cd_type),
+                         CODE_SUBTYPE=as.character(),
+                         CODE=as.character(),
+                         CODE_GRP=as.character(),
+                         stringsAsFactors=F)
+  
+  # main code body for parsing json file
+  vs_name_list<-names(vs_file)
+  vs_name_dist<-stringdist(tolower(vs_name_str),vs_name_list, method="jw")
+  vs_name_match<-vs_name_list[which.min(vs_name_dist)]
+  vs<-vs_file[[vs_name_match]]
+  for(vs_name in names(vs)){
+      for(cd_type in names(vs[["codelist"]])){
+        cd_lst<-vs[["codelist"]][[cd_type]][,1]
+        lookup_tbl %<>%
+          bind_rows(data.frame(CODE_TYPE=cd_type,
+                               CODE_TYPE_CDM=cdm_code_type_map(cd_type),
+                               CODE_SUBTYPE="exact",
+                               CODE=as.character(cd_lst),
+                               CODE_GRP=vs_name_match,
+                               stringsAsFactors = F))
+    }
+  }
+  # return data.frame
+  return(lookup_tbl)
+}
+
+load_valueset<-function(vs_template = "curated",
+                        vs_url = "",
+                        vs_name_str = "",
+                        dry_run = TRUE,
+                        conn=NULL,
+                        write_to_schema = "PUBLIC",
+                        write_to_tbl = "TEMP"){
+  vs_load_func<-get(paste0("load_valueset.",vs_template))
+  lookup_tbl<-vs_load_func(vs_url=vs_url,vs_name_str=vs_name_str)
+  
+  # run query
+  if(dry_run==TRUE){
+    return(lookup_tbl)
+  }else{
+    if(is.null(conn)){
+      stop("connection needs to be specified!")
+    }else{
+      # write valueset table to target db
+      copy_to(conn, lookup_tbl, 
+              in_schema(write_to_schema, write_to_tbl))
+    }
+  }
+}
+
+cdm_search_key<-function(cdm_tbl){
+  if(cdm_tbl=="DIAGNOSIS"){
+    return(c('DX','DX_TYPE'))
+  }else if(cdm_tbl=="CONDITION"){
+    return(c('CONDITION','CONDITION_TYPE'))
+  }else if(cdm_tbl=="PROCEDURES"){
+    return(c('PX','PX_TYPE'))
+  }else if(cdm_tbl=="LAB_RESULT_CM"){
+    return('LAB_LOINC')
+  }else if(cdm_tbl=="OBS_CLIN"){
+    return(c('OBSCLN_CODE','OBSCLN_TYPE'))
+  }else if(cdm_tbl=="PRESCRIBING"){
+    return('RXNORM_CUI')
+  }else if(cdm_tbl=="DISPENSING"){
+    return('NDC')
+  }else if(cdm_tbl=="MED_ADMIN"){
+    return(c('MEDADMIN_CODE','MEDADMIN_TYPE'))
+  }else{
+    stop("target CDM table is not searchable!")
+  }
+}
 
 collect_cdm<-function(conn,
                       patset = "PAT_ELIG",
                       key_col_pat = c("PATID"),
+                      idx_dt_col = "INDEX_DATE",
                       cdm_schema = "CMS_PCORNET_CDM",
                       key_col_schema=c("PATID"),
                       cdm_tbl = "DIAGNOSIS",
-                      idx_dt_col = "INDEX_DATE",
                       ep_dt_col = "EVENT_DATE",
+                      vs_template = "curated",
                       vs_url = "",
-                      vs_template = "curated-dx",
-                      vs_name = "",
+                      vs_name_str = "",
                       col_out = NULL,
                       write_back = FALSE,
-                      home_schema = "PUBLIC",
-                      write_tbl = "TEMP",
+                      write_to_schema = "PUBLIC",
+                      write_to_tbl = "TEMP",
                       dry_run = TRUE){
   
   # get column name of the target cdm table
@@ -98,125 +193,50 @@ collect_cdm<-function(conn,
               paste0(paste(paste0("p.",key_col_pat,collapse = ","),",",
                            paste(paste0("f.",col_out,collapse = ",")),
                            paste0(",datediff('day',p.",idx_dt_col,",f.",ep_dt_col,") as DAYS_SINCE_INDEX"),
-                           paste0(" from ", home_schema, ".", patset, " p"),
+                           paste0(" from ", write_to_schema, ".", patset, " p"),
                            paste0(" join ", cdm_schema, ".", cdm_tbl," f"),
                            " on ",match_key)))
   
-  vs_file_type<-gsub(".*\\.","",vs_url)
+  # load valueset as a data.frame
+  lookup_tbl<-load_valueset(vs_url = vs_url,
+                            vs_template = vs_template,
+                            vs_name_str = vs_name_str)
+  
   # specify "where" clause based on valuset type
-  if (vs_file_type == "json"){
-    vs<-jsonlite::fromJSON(vs_url)[[vs_name]]
-    # curated valueset of icd diagnosis codes
-    if (vs_template == "curated-dx"){
-      sql_where<-""      
-      for (icd_vrsn in c("icd9","icd10")){
-        for(lev_i in 0:2){
-          icd_lev<-paste0("lev",lev_i)
-          if (lev_i==0&&length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where," or split_part(f.DX,'.',1) in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
-          }else if(length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where," or substr(f.DX,1,",4+lev_i,") in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
-          }
-        }
+  search_key<-cdm_search_key(cdm_tbl)
+  dx_ind<-as.numeric(grepl("DX",search_key[1]))
+  cdtype_scan<-lookup_tbl %>% select(CODE_TYPE) %>% unique %>% unlist
+  cdsubtype_scan<-lookup_tbl %>% select(CODE_SUBTYPE) %>% unique %>% unlist
+  sql_where<-list()
+  for(type in cdtype_scan){
+    sql_where_sub<-""
+    for(subtype in cdsubtype_scan){
+      search_val<-lookup_tbl %>%
+        filter(CODE_TYPE==type & CODE_SUBTYPE==subtype) %>%
+        select(CODE, CODE_TYPE_CDM) %>% unique
+      if(nrow(search_val)==0){
+        next
+      }else{
+        search_key_val=search_val$CODE_TYPE_CDM[1]
       }
-      sql<-paste0(sql," and (",gsub("^( or)+","",sql_where),")")
-      
-    # curated valueset of cpt or icd procedure codes
-    } else if (vs_template == "curated-px"){
-      # icd pcs codes
-      sql_where<-"" 
-      for(icd_vrsn in c("icd9-pcs","icd10-pcs")){
-        for(lev_i in 0:2){
-          icd_lev<-paste0("lev",lev_i)
-          if(lev_i==0&&length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where," or split_part(f.PX,'.',1) in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
-          }else if(length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where," or substr(f.PX,1,",3+lev_i,") in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
-          }
-        }
-      # cpt codes
-      for(type in c("range","list")){
-          if(type=="range"&&length(vs[[icd_vrsn]][[icd_lev]])>0){
-            rg2vec<-lapply(vs[["hcpcs"]][["range"]],
-                           function(x)seq(as.numeric(strsplit(x,"-")[[1]][1]),as.numeric(strsplit(x,"-")[[1]][2])))
-            sql_where<-paste0(sql_where," or f.PX in ", 
-                         paste0("('",paste(unlist(rg2vec),collapse="','"),"')"))
-          }else if(length(vs[[icd_vrsn]][[icd_lev]])>0){
-            sql_where<-paste0(sql_where," or f.PX in ", 
-                         paste0("('",paste(vs[[icd_vrsn]][[icd_lev]],collapse="','"),"')"))
-          }
-        }
+      if(subtype=="lev0"){
+        sql_where_sub<-paste0(sql_where_sub," or split_part(f.",search_key[1],",'.',1) in ", 
+                              paste0("('",paste(search_val$CODE,collapse="','"),"')"))
+      }else if(subtype %in% c("lev1","lev2")){
+        lev_i<-as.numeric(gsub("lev","",subtype))
+        sql_where_sub<-paste0(sql_where_sub," or substr(f.",search_key[1],",1,",3+dx_ind+lev_i,") in ", 
+                              paste0("('",paste(search_val$CODE,collapse="','"),"')"))
+      }else{
+        sql_where_sub<-paste0(sql_where_sub,"f.",search_key[1]," in ",
+                              paste0("('",paste(search_val$CODE,collapse="','"),"')"))
       }
-      sql<-paste0(sql," and (",gsub("^( or)+","",sql_where),")")
-    
-    # eCQM published valuesets
-    } else if (vs_template == "ecqm") {
-      for(cdtype in names(vs[["codelist"]])){
-        codelist<-paste0("('",paste(vs[["codelist"]][[cdtype]][,1],collapse="','"),"')")
-        # diagnosis
-        if(grepl("^(ICD)+",cdtype)){
-          if(cdm_table=="DIAGNOSIS"){
-            sql<-paste0(sql," and f.DX in ",codelist)
-          }else if(cdm_table=="CONDITION"){
-            sql<-paste0(sql," and f.CONDITION in ",codelist)
-          }
-        # procedures
-        }else if((grepl("^((CPT)|(HCPCS))+",cdtype))){
-          if(cdm_table=="PROCEDURES"){
-            sql<-paste0(sql," and f.PX in ",codelist)
-          }
-        # lab
-        }else if(grepl("^(LOINC)+",cdtype)){
-          if(cdm_table=="LAB_RESULT_CM"){
-            sql<-paste0(sql," and f.LAB_LOINC in ",codelist)
-          }else if(cdm_table=="OBS_CLIN"){
-            sql<-paste0(sql," and f.OBSLINC_CODE in ",codelist,
-                        "where OBSCLIN_TYPE ='LC'")
-          }
-        # meds
-        }else if(grepl("^(RXNORM)+",cdtype)){
-          if(cdm_table=="PRESCRIBING"){
-            ssql<-paste0(sql," and f.RXNORM_CUI in ",codelist)
-          }else if(cdm_table=="MED_ADMIN"){
-            sql<-paste0(sql," and f.MEDADMIN_CODE in ",codelist,
-                      "where MEDADMIN_TYPE = 'RX'")
-          }          
-        # obs_clin
-        }else if(grepl("^(SNOMED)+",cdtype)){
-          if(cdm_table=="DIAGNOSIS"){
-            sql<-paste0(sql," and f.DX in ",codelist,
-                      "where DX_TYPE = 'SM'")
-          }else if(cdm_table=="CONDITION"){
-            sql<-paste0(sql," and f.CONDITION in ",codelist,
-                      "where CONDITION_TYPE = 'SM'")
-          }else if(cdm_table=="OBS_CLIN"){
-            sql<-paste0(sql," and f.OBSLINC_CODE in ",codelist,
-                        "where OBSCLIN_TYPE ='SM'")
-          }  
-        }
-      }
-      
-    # CCDA published valuesets
-    } else if (vs_template == "ccda"){
-      # TODO
-    
-    # bioportal search results
-    } else if (vs_template == "ncbo"){
-      # TODO
-      
-    # UMLS API search results
-    } else if (vs_template == "umls"){
-      # TODO
-    } else {
-      stop("valueset file type not supported!")
     }
-  } else if (vs_file_type == "csv"){
-    #TODO
+    sql_where[[type]]<-gsub("^( or)+","",sql_where_sub)
+    if(length(search_key)>1){
+      sql_where[[type]]<-paste0(sql_where[[type]]," and f.",search_key[2],"='",search_key_val,"'")
+    }
   }
+  sql<-paste0(sql," where ",paste0("(",paste(unlist(sql_where),collapse=") or ("),")"))
   
   # run query
   if(dry_run==TRUE){
@@ -225,7 +245,7 @@ collect_cdm<-function(conn,
     if(write_back==TRUE){
       DBI::dbSendQuery(conn,
                        paste0("create or replace table ",
-                              home_schema,".",write_tbl," as ",sql))
+                              write_to_schema,".",write_tbl," as ",sql))
     }else{
       DBI::dbGetQuery(conn,sql)
     }
---
+++ testthis.R
@@ -0,0 +1,82 @@
+install.packages("jsonlite")
+install.packages("stringdist")
+
+library(jsonlite)
+library(DBI)
+library(magrittr)
+library(tidyverse)
+library(stringdist)
+
+load_valueset.curated(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+                      vs_name_str = "myocardial infarction")
+
+load_valueset.curated(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+                      vs_name_str = "peripheral")
+
+load_valueset.curated(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+                      vs_name_str = "cerebrovascular")
+
+
+load_valueset.curated(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+                      vs_name_str = "bariatric surgery")
+
+load_valueset.ecqm(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_autogen/ecqm-allergy-intolerance.json",
+                   vs_name_str = "statin")
+
+
+load_valueset.curated(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_autogen/ecqm-medication.json",
+                      vs_name_str = "warfarin")
+
+
+load_valueset(vs_template = "curated",
+              vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+              vs_name_str = "myocardial infarction")
+
+
+# load_valueset(vs_template = "curated",
+#               vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+#               vs_name_str = "myocardial infarction",
+#               dry_run = FALSE,
+#               conn=myconn,
+#               write_to_schema = "PUBLIC",
+#               write_to_tbl = "TEMP")
+
+source("extract_util.R")
+collect_cdm(conn=NULL,
+            patset = "PAT_ELIG",
+            key_col_pat = c("PATID"),
+            idx_dt_col = "INDEX_DATE",
+            cdm_schema = "CMS_PCORNET_CDM",
+            key_col_schema=c("PATID"),
+            cdm_tbl = "DIAGNOSIS",
+            ep_dt_col = "EVENT_DATE",
+            vs_template = "curated",
+            vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+            vs_name_str = "myocardial infarction",
+            col_out = c("DX","DX_DATE"),
+            write_back = FALSE,
+            write_to_schema = "OBESITY",
+            write_to_tbl = "TEMP",
+            dry_run = TRUE)
+
+
+source("extract_util.R")
+load_valueset.curated(vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+                      vs_name_str = "bariatric surgery")
+collect_cdm(conn=NULL,
+            patset = "PAT_ELIG",
+            key_col_pat = c("PATID"),
+            idx_dt_col = "INDEX_DATE",
+            cdm_schema = "CMS_PCORNET_CDM",
+            key_col_schema=c("PATID"),
+            cdm_tbl = "PROCEDURES",
+            ep_dt_col = "EVENT_DATE",
+            vs_template = "curated",
+            vs_url = "https://raw.githubusercontent.com/sxinger/PheCDM/main/valusets/valueset_curated/vs-osa-comorb.json",
+            vs_name_str = "bariatric surgery",
+            col_out = c("PX","PX_DATE"),
+            write_back = FALSE,
+            write_to_schema = "OBESITY",
+            write_to_tbl = "TEMP",
+            dry_run = TRUE)
+
