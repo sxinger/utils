f875a9ad20152fbc9b62bb85ebd84a3417dbfdae
Song <xsm7f@umsystem.edu>
2023-02-13 00:21:05 -0600

    add remove_libraries

--- setup_util.R
+++ setup_util.R
@@ -6,7 +6,7 @@
 #   #create .Renviron file 
 # }
 
-##install and load multiple libraries (equivalent to pacman::p_load)
+## install and load multiple libraries (equivalent to pacman::p_load)
 require_libraries<-function(package_list,verb=T){
   for (lib in package_list) {
     chk_install<-!(lib %in% installed.packages()[,"Package"])
@@ -18,6 +18,21 @@ require_libraries<-function(package_list,verb=T){
       cat("\n", lib, " loaded.", sep="") 
     }
   }
+}
+
+## remove all 
+remove_libraries<-function(pkg_lst=c()){
+  # create a list of all installed packages
+  ip <- as.data.frame(installed.packages()) %>%
+    # if you use MRO, make sure that no packages in this library will be removed
+    filter(!grepl("MRO", LibPath)) %>%
+    # we don't want to remove base or recommended packages
+    filter(Priority %in% c("base", "recommended"))
+  if (length(pkg_lst) > 0 ){
+    ip %<>% filter(Package %in% pkg_lst)
+  }
+  # remove the packages
+  sapply(ip$Package, remove.packages, lib = ip$LibPath)
 }
 
 ## dual-axis utility function
