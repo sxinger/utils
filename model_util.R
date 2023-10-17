d695aac96a07782363688bed9b607292711fb1f6
Song <xsm7f@umsystem.edu>
2023-10-16 23:15:07 -0500

    add prune_xgb

--- model_util.R
+++ model_util.R
@@ -202,11 +202,132 @@ fast_rfe.coxph<-function(
   return(out)
 }
 
+prune_xgb<-function(
+  dtrain,
+  dtest,
+  params=list(
+    booster = "gbtree",
+    max_depth = 10,
+    min_child_weight = 2,
+    colsample_bytree = 0.8,
+    subsample = 0.7,
+    eta = 0.05,
+    lambda = 1,
+    alpha = 0,
+    gamma = 1,
+    objective = "binary:logistic",
+    eval_metric = "auc"
+  ),
+  nround = 500,
+  folds = folds,
+  # nfold = 5,
+  prediction = FALSE,
+  showsd = FALSE,
+  early_stopping_rounds = 50,
+  maximize = TRUE,
+  verbose = 1,
+  print_every_n=50
+
+){
+  #--determine number of trees, or steps (more rounds, early stopping)
+  bst <- xgb.cv(
+    params = list(
+      booster = params$booster,
+      max_depth = params$max_depth
+      min_child_weight = params$min_child_weight,
+      colsample_bytree = params$colsample_bytree,
+      subsample = params$subsample,
+      eta = params$eta,
+      lambda = params$lambda,
+      alpha = params$alpha,
+      gamma = params$gamma,
+      objective = params$objective,
+      eval_metric = params$eval_metric
+),
+    data = dtrain,
+    nround = nrounds,
+    folds = folds,
+    # nfold = nfold,
+    prediction = prediction,
+    showsd = showsd,
+    early_stopping_rounds = early_stopping_rounds,
+    maximize = maximize,
+    verbose = verbose,
+    print_every_n = print_every_n
+  ) 
+  # get optimal steps
+  steps<-which(bst$evaluation_log$test_auc_mean==max(bst$evaluation_log$test_auc_mean))
+  
+  #----------- benchmarking -------------------
+  lapse_i<-Sys.time()-start_tsk_i
+  bm<-c(bm,paste0(round(lapse_i,1),units(lapse_i)))
+  bm_nm<-c(bm_nm,"tune model")
+  if(verbose==1){
+    cat(paste0(c(pred_in_d,pred_task,fs_type),collapse = ","),
+      "...finish model tunning.\n")
+  }
+  start_tsk_i<-Sys.time() 
+  #----------- benchmarking -------------------
+
+  #--prune the optimal model
+  xgb_tune<-xgb.train(
+    data=dtrain,
+    max_depth = params$max_depth,
+    min_child_weight = params$min_child_weight,
+    colsample_bytree = params$colsample_bytree,
+    subsample = params$subsample,
+    eta = params$eta,
+    maximize = maximize,
+    nrounds = steps,
+    eval_metric = params$eval_metric,
+    objective = params$objective,
+    verbose = verbose
+  )
+  
+  #--collect testing results
+  valid<-data.frame(
+    actual = ,
+    pred = predict(xgb_tune,dtest),
+    stringsAsFactors = F
+  )
+  
+  #--feature importance
+  feat_imp<-xgb.importance(model=xgb_tune)
+  
+  #----------- benchmarking -------------------  
+  lapse_i<-Sys.time()-start_tsk_i
+  bm<-c(bm,paste0(round(lapse_i,1),units(lapse_i)))
+  bm_nm<-c(bm_nm,"validate model")
+  if(verbose==1){
+    cat(paste0(c(pred_in_d,pred_task,fs_type),collapse = ","),
+        "...finish model validating.\n")
+   }
+  #----------- benchmarking -------------------
+
+  #-----------save model and other results--------
+  result<-list(
+    model = xgb_tune,
+    pred_df = valid,
+    feat_imp = feat_imp
+  )
+    
+  #----------- benchmarking -------------------
+  lapse_tsk<-Sys.time()-start_tsk
+  bm<-c(bm,paste0(round(lapse_tsk,1),units(lapse_tsk)))
+  bm_nm<-c(bm_nm,"complete task")
+  if(verbose==1){  
+    cat("\nFinish building reference models for task:",pred_task,"in",pred_in_d,"with",fs_type,",in",lapse_tsk,units(lapse_tsk),
+     ".\n--------------------------\n")
+  }
+  #----------- benchmarking -------------------
+
+  return(result)
+}
+
 bayeopt_xgb<-function(
-  trainX,
-  trainY,
-  testX,
-  testY,
+  dtrain,
+  dtest,
+  folds=5,
   params_bd=list(
     max_depth = c(4L, 10L),
     min_child_weight = c(2L,10L),
@@ -228,15 +349,11 @@ bayeopt_xgb<-function(
     cl <- makeCluster(N_CL)
     registerDoParallel(cl)
     clusterExport(cl,'df') # copying data to clusters (note:xgb.DMatrix is not compatible with parallelization)
-    clusterEvalQ(cl,expr= {                          # copying model to clusters
-      library(xgboost)
+    clusterEvalQ(cl,expr= {                          
+      library(xgboost) # copying model to clusters
     })
   }
-  
-  #--covert to xgb data frame
-  dtrain<-xgb.DMatrix(data=trainX,label=trainY)
-  dtest<-xgb.DMatrix(data=testX,label=testY)
-  
+ 
   #-----------------------------------------------benchmark-----------------------------------------#
   lapse_i<-Sys.time()-start_tsk_i
   bm<-c(bm,paste0(round(lapse_i,1),units(lapse_i)))
@@ -268,7 +385,7 @@ bayeopt_xgb<-function(
       ),
         data = dtrain,
         nround = 100,
-        folds = 5,
+        folds = folds,
         prediction = FALSE,
         # showsd = TRUE,
         early_stopping_rounds = 5,
@@ -294,86 +411,11 @@ bayeopt_xgb<-function(
   Best_Par<-getBestPars(OPT_Res)
   
   #--stop cluster
-  stopCluster(cl)
-  registerDoSEQ()
-  
-  #--determine number of trees, or steps (more rounds, early stopping)
-  dtrain<-xgb.DMatrix(data=X_tr_sp,label=y_tr_sp$y)
-  bst <- xgb.cv(params = list(booster = "gbtree",
-                              max_depth = Best_Par$max_depth,
-                              min_child_weight = Best_Par$min_child_weight,
-                              colsample_bytree = Best_Par$colsample_bytree,
-                              subsample=0.7,
-                              eta=0.05,
-                              lambda=1,
-                              alpha=0,
-                              gamma=1,
-                              objective = "binary:logistic",
-                              eval_metric = "auc"),
-                data = dtrain,
-                nround = 500,
-                folds = folds,
-                # nfold=5,
-                prediction = FALSE,
-                # showsd = TRUE,
-                early_stopping_rounds = 50,
-                maximize = TRUE,
-                verbose = 1,
-                print_every_n=50) 
-  
-  steps<-which(bst$evaluation_log$test_auc_mean==max(bst$evaluation_log$test_auc_mean))
-  
-  lapse_i<-Sys.time()-start_tsk_i
-  bm<-c(bm,paste0(round(lapse_i,1),units(lapse_i)))
-  bm_nm<-c(bm_nm,"tune model")
-  
-  cat(paste0(c(pred_in_d,pred_task,fs_type),collapse = ","),
-      "...finish model tunning.\n")
-  
-  #-----------validate model------------
-  start_tsk_i<-Sys.time() 
-  
-  #--validation
-  xgb_tune<-xgb.train(data=dtrain,
-                      max_depth = Best_Par$max_depth,
-                      min_child_weight = Best_Par$min_child_weight,
-                      colsample_bytree = Best_Par$colsample_bytree,
-                      subsample=0.7,
-                      eta=0.05,
-                      maximize = TRUE,
-                      nrounds=steps,
-                      eval_metric="auc",
-                      objective="binary:logistic",
-                      verbose = 0)
-  
-  valid<-data.frame(y_ts_sp,
-                    pred = predict(xgb_tune,dtest),
-                    stringsAsFactors = F)
-  
-  #--feature importance
-  feat_imp<-xgb.importance(model=xgb_tune)
-  
-  lapse_i<-Sys.time()-start_tsk_i
-  bm<-c(bm,paste0(round(lapse_i,1),units(lapse_i)))
-  bm_nm<-c(bm_nm,"validate model")
-  
-  cat(paste0(c(pred_in_d,pred_task,fs_type),collapse = ","),
-      "...finish model validating.\n")
-  
-  #-----------save model and other results--------
-  result<-list(hyper_param=c(Best_Par,steps),
-               model=xgb_tune,
-               pred_df=valid,
-               feat_imp=feat_imp)
-  
-  #-------------------------------------------------------------------------------------------------------------
-  lapse_tsk<-Sys.time()-start_tsk
-  bm<-c(bm,paste0(round(lapse_tsk,1),units(lapse_tsk)))
-  bm_nm<-c(bm_nm,"complete task")
-  
-  cat("\nFinish building reference models for task:",pred_task,"in",pred_in_d,"with",fs_type,",in",lapse_tsk,units(lapse_tsk),
-      ".\n--------------------------\n")
-  
+  if(N_CL > 1){
+    stopCluster(cl)
+    registerDoSEQ()
+  }
+ 
   #benchmark
   bm<-data.frame(bm_nm=bm_nm,bm_time=bm,
                  stringsAsFactors = F)
